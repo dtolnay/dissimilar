56fe26f75ebf5d29a89f7b986db62be521622cd4
David Tolnay <dtolnay@gmail.com>
2022-12-17 10:55:13 -0800

    Release 1.0.5

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@
 //! [Myers' diff algorithm]: https://neil.fraser.name/writing/diff/myers.pdf
 //! [semantic cleanups]: https://neil.fraser.name/writing/diff/
 
-#![doc(html_root_url = "https://docs.rs/dissimilar/1.0.4")]
+#![doc(html_root_url = "https://docs.rs/dissimilar/1.0.5")]
 #![allow(
     clippy::blocks_in_if_conditions,
     clippy::bool_to_int_with_if,
