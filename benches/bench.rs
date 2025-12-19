c9fe71c4f4c3937fb9a1c9bc95531744293db589
David Tolnay <dtolnay@gmail.com>
2025-12-18 18:30:42 -0800

    Switch from cargo bench to criterion

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,15 +1,13 @@
-#![feature(test)]
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::fs;
 
-extern crate test;
-
-use dissimilar::diff;
-use std::{fs, io};
-use test::Bencher;
-
-#[bench]
-fn bench(b: &mut Bencher) -> io::Result<()> {
-    let document1 = fs::read_to_string("benches/document1.txt")?;
-    let document2 = fs::read_to_string("benches/document2.txt")?;
-    b.iter(|| diff(&document1, &document2));
-    Ok(())
+fn bench(c: &mut Criterion) {
+    c.bench_function("dissimilar::diff", |b| {
+        let document1 = fs::read_to_string("benches/document1.txt").unwrap();
+        let document2 = fs::read_to_string("benches/document2.txt").unwrap();
+        b.iter(|| dissimilar::diff(&document1, &document2));
+    });
 }
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
